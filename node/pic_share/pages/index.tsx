fba1e83cce3ea2bbcbfdda346d3761344b531d78
jun <junpeeei0407@gmail.com>
2022-10-23 11:39:41 +0900

    tailwindcss インスール

--- node/pic_share/pages/index.tsx
+++ node/pic_share/pages/index.tsx
@@ -1,13 +1,20 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
 
 import BasicButtons from '../components/atoms/BasicButtons'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <p>test</p>
       <BasicButtons />
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Link href='/accounts/login'>
+        ログイン
+      </Link>
     </div>
   )
 }
